8e5e33a2e20efbf2089e5ed8c5ca9a8396810c0b
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-15 17:49:20 +0200

    add footer under

--- src/components/FooterUnder.vue
+++ src/components/FooterUnder.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>
-        <h1>
-            FooterUnder
-        </h1>
+    <div class="background">
+        <div class="container">
+            <h2>SIGN-UP NOW!</h2>
+            <nav>
+                <ul>
+                    <li class="active">FOLLOW US</li>
+                    <li><img src="../assets/footer-facebook.png" alt="Facebook"></li>
+                    <li><img src="../assets/footer-twitter.png" alt="Twitter"></li>
+                    <li><img src="../assets/footer-youtube.png" alt="Youtube"></li>
+                    <li><img src="../assets/footer-pinterest.png" alt="Pinterest"></li>
+                    <li><img src="../assets/footer-periscope.png" alt="periscope"></li>
+                </ul>
+            </nav>
+        </div>
+        
     </div>
+    
 </template>
 <script>
 export default {
@@ -18,4 +30,30 @@ export default {
 <style lang="scss" scoped>
     @use '../styles/partials/mixins' as*;
     @use '../styles/partials/variables' as*;
+
+    div.background{
+        background-color: $thirdbgColor;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 140px;
+    }
+
+    ul{
+        display: flex;
+
+        li.active{
+            font-size: 1.7rem;
+            color: $primaryColor;
+        }
+    }
+
+    h2{
+        border: 1px $primaryColor solid;
+        padding: 1rem;
+        font-size: 1.3rem;
+        color: $thirdColor;
+        margin-left: 1rem;
+        
+    }
 </style>
